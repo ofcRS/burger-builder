050ad4bdead3b954b7ec3d3bcccb13a34bfb69cc
ofcRS <azeric054@gmail.com>
2018-11-25 11:05:18 +0400

    Create a Burger container. Add Layout.css as well.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 
+import classes from './Layout.css';
+
 const layout = (props) => {
   return (
     <Aux>
       <div>ToolBar, SideDrawer, Backdrop</div>
-      <main>
+      <main className={classes.Content}>
         {props.children}
       </main>
     </Aux>
