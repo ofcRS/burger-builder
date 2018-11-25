d6cf978b3aaf735b8fc181149bfa60c8937de7d1
ofcRS <azeric054@gmail.com>
2018-11-25 17:07:48 +0400

    BuildControls components are dynamicly now

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,19 +4,51 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENTS_COSTS = {
+  cheese: 0.8,
+  bacon: 1.6,
+  salad: 0.3,
+  meat: 2.4
+}
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
       cheese: 0,
-      bacon: 0
-    }
+      bacon: 0,
+      salad: 0,
+      meat: 0
+    },
+    totalPrice: 4
   };
+
+  addIngredientHandler = (type) => {
+    const newIngredients = {...this.state.ingredients};
+    newIngredients[type] += 1;
+    const newTotalPrice = this.state.totalPrice + INGREDIENTS_COSTS[type];
+    this.setState({
+      ingredients: newIngredients,
+      totalPrice: newTotalPrice
+    })
+  };
+
+  removeIngredientHandler = (type) => {
+    const newIngredients = {...this.state.ingredients};
+    newIngredients[type] -= 1;
+    const newTotalPrice = this.state.totalPrice - INGREDIENTS_COSTS[type];
+    this.setState({
+      ingredients: newIngredients,
+      totalPrice: newTotalPrice
+    })
+  }
 
   render() {
     return (
       <Aux>
-        <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler} />
       </Aux>
     )
   }
