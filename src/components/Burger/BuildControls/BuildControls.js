b7d075cc383913361bc28e1165291651e81e2d75
ofcRS <azeric054@gmail.com>
2018-11-26 00:15:02 +0400

    Implement a ordering modal with couple of UI components

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,6 +12,7 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>The current price: <strong>{props.price.toFixed(2)}$</strong></p>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
@@ -20,8 +21,11 @@ const buildControls = (props) => (
         added={() => props.ingredientAdded(ctrl.type)}
         removed={() => props.ingredientRemoved(ctrl.type)}
         disabled={props.disabled[ctrl.type]} />
-
     ))}
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.purchasing}>Order now</button>
   </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENTS_COSTS = {
   cheese: 0.8,
@@ -14,36 +16,62 @@ const INGREDIENTS_COSTS = {
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      cheese: 0,
       bacon: 0,
-      salad: 0,
-      meat: 0
+      cheese: 0,
+      meat: 0,
+      salad: 0
     },
-    totalPrice: 4
+    totalPrice: 3.5,
+    purchasable: false,
+    purchasing: false
   };
 
+  purchaseHandler = () => {
+    this.setState({
+      purchasing: true
+    });
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false
+    });
+  }
+
+  purchaseContinueHandler = () => {
+    alert('You continue!')
+  }
+
   addIngredientHandler = (type) => {
-    const newIngredients = {...this.state.ingredients};
+    const newIngredients = {
+      ...this.state.ingredients
+    };
     newIngredients[type] += 1;
     const newTotalPrice = this.state.totalPrice + INGREDIENTS_COSTS[type];
-    this.setState({
-      ingredients: newIngredients,
-      totalPrice: newTotalPrice
-    })
+    this.setState({ingredients: newIngredients, totalPrice: newTotalPrice});
+    this.updatePurchasable(newIngredients);
   };
 
   removeIngredientHandler = (type) => {
-    const newIngredients = {...this.state.ingredients};
+    const newIngredients = {
+      ...this.state.ingredients
+    };
     newIngredients[type] -= 1;
     if (newIngredients[type] < 0) {
       return;
     }
     const newTotalPrice = this.state.totalPrice - INGREDIENTS_COSTS[type];
-    this.setState({
-      ingredients: newIngredients,
-      totalPrice: newTotalPrice
-    })
+    this.setState({ingredients: newIngredients, totalPrice: newTotalPrice});
+    this.updatePurchasable(newIngredients);
   }
+
+  updatePurchasable(ingredients) {
+    this.setState({
+      purchasable: (Object.values(ingredients).reduce((sum, el) => {
+        return sum + el
+      }, 0)) > 0
+    })
+  };
 
   render() {
     const disabledInfo = {
@@ -56,11 +84,22 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCanceled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}/>
+        </Modal>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
-          disabled={disabledInfo} />
+          price={this.state.totalPrice}
+          disabled={disabledInfo}
+          purchasable={this.state.purchasable}
+          purchasing={this.purchaseHandler}/>
       </Aux>
     )
   }
