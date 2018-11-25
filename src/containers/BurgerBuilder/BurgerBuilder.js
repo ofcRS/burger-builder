a40f173d05ba391fe88e9945c86ab49c2b8273cd
ofcRS <azeric054@gmail.com>
2018-11-25 17:33:01 +0400

    Fix bug ... Now button disable when you try make a negative count of any ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -35,6 +35,9 @@ class BurgerBuilder extends Component {
   removeIngredientHandler = (type) => {
     const newIngredients = {...this.state.ingredients};
     newIngredients[type] -= 1;
+    if (newIngredients[type] < 0) {
+      return;
+    }
     const newTotalPrice = this.state.totalPrice - INGREDIENTS_COSTS[type];
     this.setState({
       ingredients: newIngredients,
@@ -43,12 +46,21 @@ class BurgerBuilder extends Component {
   }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    }
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = (disabledInfo[key] === 0);
+    }
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
-          ingredientRemoved={this.removeIngredientHandler} />
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo} />
       </Aux>
     )
   }
