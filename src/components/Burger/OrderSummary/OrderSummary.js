c5860e024028f4e5d886f698bf8d37df2c2d8926
ofcRS <azeric054@gmail.com>
2018-12-16 07:22:55 +0400

    Implement Toolbar and Sidebar. Optimize them for mobile devices

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,7 @@ const orderSummary = (props) => {
       <h3>Your order: </h3>
       <p>A delicious burger with the following ingredients:</p>
       {ingredientSummary}
+      <p><strong>TotalPrice: {props.price.toFixed(2)} $</strong></p>
       <p>Continue to checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
       <Button btnType="Success" clicked={props.purchaseContinued}>Continue!</Button>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,6 +88,7 @@ class BurgerBuilder extends Component {
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}>
           <OrderSummary
+            price={this.state.totalPrice}
             ingredients={this.state.ingredients}
             purchaseCanceled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}/>
