eb56dd94aba24781d80f2b6b69b41a61ae7acd56
ofcRS <azeric054@gmail.com>
2019-02-10 01:09:04 +0400

    fix app for non-redux-devtools browsers

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = process.env.NODE_ENV !== 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
